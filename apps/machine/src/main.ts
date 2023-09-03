2bd85b49c90b2414ea3799ce7c72b0a81437f9b7
NAOT0 <NAOT0@example.com>
2023-09-03 16:23:34 +0900

    削除

--- apps/machine/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { MachineModule } from './machine.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(MachineModule);
-  await app.listen(3000);
-}
-bootstrap();
