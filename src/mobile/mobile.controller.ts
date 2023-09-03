bb4a85ddde2e45820ae0428c6fd7f7876fc1eea5
NAOT0 <NAOT0@example.com>
2023-09-03 19:34:13 +0900

    /:id追加

--- src/mobile/mobile.controller.ts
+++ src/mobile/mobile.controller.ts
@@ -1,14 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { MobileService } from './mobile.service';
 
 @Controller('mobile')
 export class MobileController {
-    constructor(private readonly mobileService: MobileService){}
+    constructor(private readonly mobileService: MobileService) { }
 
     @Get("messages")
-    getMessages(): string[]{
-        return this.mobileService.getMessages();     
+    getMessages(): string[] {
+        return this.mobileService.getMessages();
     }
 
+    @Get("messages/:id")
+    getMessageById(@Param("id") id: string): string {
+        return `This action returns a #${id} message`;
+    }
 
 }
